3845b140322eebc84100a6445ca4e55ed188d3df
Franco Nahuel Rupp <ruppfn@gmail.com>
2022-09-21 02:53:42 -0300

    change lambda runtime

--- terraform/cv-lambda.tf
+++ terraform/cv-lambda.tf
@@ -10,7 +10,7 @@ resource "aws_lambda_function" "cv_lambda" {
   role             = aws_iam_role.lambda_role.arn
   handler          = "cv-lambda"
   source_code_hash = filebase64sha256(data.archive_file.cv_lambda_zip.output_path)
-  runtime          = "go1.x"
+  runtime          = "provided.al2"
   architectures    = ["arm64"]
   memory_size      = 128
   timeout          = 30
