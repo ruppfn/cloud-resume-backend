1c9402953d50ed6295cba1803eef82459af0383b
Franco Nahuel Rupp <ruppfn@gmail.com>
2022-09-24 23:41:20 -0300

    fix format

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,7 +1,7 @@
 resource "aws_dynamodb_table" "dynamo_table" {
-  name = "CloudResume"
-  billing_mode = "PROVISIONED"
-  read_capacity = 5
+  name           = "CloudResume"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 5
   write_capacity = 5
 
   hash_key = "id"
@@ -14,6 +14,6 @@ resource "aws_dynamodb_table" "dynamo_table" {
 
 resource "aws_dynamodb_table_item" "dynamo_cv_item" {
   table_name = aws_dynamodb_table.dynamo_table.name
-  hash_key = aws_dynamodb_table.dynamo_table.hash_key
-  item = file("../cv-db-model.json")
+  hash_key   = aws_dynamodb_table.dynamo_table.hash_key
+  item       = file("../cv-db-model.json")
 }
