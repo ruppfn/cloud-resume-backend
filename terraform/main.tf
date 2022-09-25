873f3c2ba15920849ced521c15cc68306f7e4c72
Franco Nahuel Rupp <ruppfn@gmail.com>
2022-09-25 03:28:35 -0300

    add missing api. to domain name

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ locals {
   region      = "us-east-2"
   account_id  = data.aws_caller_identity.current.account_id
   build_path  = "../build"
-  domain_name = "${var.environment == "develop" ? "dev." : ""}frupp.com.ar"
+  domain_name = "api.${var.environment == "develop" ? "dev." : ""}frupp.com.ar"
 }
 
 terraform {
