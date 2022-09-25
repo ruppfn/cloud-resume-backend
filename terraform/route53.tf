c3c8b6a11b005b10cde8909e5da44528e8cd2b5a
Franco Nahuel Rupp <ruppfn@gmail.com>
2022-09-25 03:11:01 -0300

    fix acm certificate validation ?

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -13,21 +13,26 @@ resource "aws_acm_certificate_validation" "validation" {
 }
 
 resource "aws_route53_record" "record" {
-  zone_id = var.zone_id
-  name    = local.domain_name
-  type    = "A"
-
-  alias {
-    evaluate_target_health = false
-    name                   = aws_apigatewayv2_domain_name.domain_name.domain_name_configuration[0].target_domain_name
-    zone_id                = aws_apigatewayv2_domain_name.domain_name.domain_name_configuration[0].hosted_zone_id
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
   }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = var.zone_id
 }
 
 resource "aws_apigatewayv2_domain_name" "domain_name" {
   domain_name = local.domain_name
   domain_name_configuration {
-    certificate_arn = aws_acm_certificate.cert.arn
+    certificate_arn = aws_acm_certificate_validation.validation.certificate_arn
     endpoint_type   = "REGIONAL"
     security_policy = "TLS_1_2"
   }
