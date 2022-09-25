99ff5443f43928a9dc1208996f8685c1eb19a870
Franco Nahuel Rupp <ruppfn@gmail.com>
2022-09-25 20:40:24 -0300

    fix cloudfront validation record

--- terraform/cloudfront_certificate.tf
+++ terraform/cloudfront_certificate.tf
@@ -22,7 +22,7 @@ resource "aws_acm_certificate_validation" "cloudfront_validation" {
 resource "aws_route53_record" "cloudfront_validation_record" {
   provider = aws.us_east_1
   for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.cloudfront_cert.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
