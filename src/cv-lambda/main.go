7a5b3970e9cf52fbdb56dcf2060b7910ad28d01f
Franco Nahuel Rupp <ruppfn@gmail.com>
2022-09-25 02:14:46 -0300

    change lambda to use API Gateway proxy response

--- src/cv-lambda/main.go
+++ src/cv-lambda/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/ruppfn/cloud-resume-backend/src/cv-lambda/process"
+	"net/http"
 	"os"
 )
 
@@ -16,15 +19,13 @@ var tableName = os.Getenv("TABLE_NAME")
 var key = os.Getenv("KEY")
 var value = os.Getenv("VALUE")
 
-func HandleRequest(ctx context.Context) (process.DynamoData, error) {
-	var dynamoData process.DynamoData
-
+func HandleRequest(ctx context.Context) (events.APIGatewayProxyResponse, error) {
 	if dynamodbClient == nil {
 		fmt.Println("initializing dynamodbClient")
 		newClient, err := process.GetDynamoClient(region, ctx)
 		if err != nil {
 			fmt.Println("Couldn't get the dynamo client")
-			return dynamoData, err
+			return internalServerError(err)
 		}
 		dynamodbClient = newClient
 	}
@@ -32,16 +33,33 @@ func HandleRequest(ctx context.Context) (process.DynamoData, error) {
 	output, err := process.GetItemFromTable(dynamodbClient, tableName, key, value, ctx)
 	if err != nil {
 		fmt.Println("Couldn't find the item")
-		return dynamoData, err
+		return internalServerError(err)
 	}
+
+	var dynamoData process.DynamoData
 
 	err = attributevalue.UnmarshalMap(output.Item, &dynamoData)
 	if err != nil {
 		fmt.Println("Couldn't unmarshal the result")
-		return dynamoData, err
+		return internalServerError(err)
 	}
 
-	return dynamoData, nil
+	jsonData, err := json.Marshal(dynamoData)
+
+	response := events.APIGatewayProxyResponse{
+		Body:            string(jsonData),
+		StatusCode:      http.StatusOK,
+		IsBase64Encoded: false,
+	}
+
+	return response, nil
+}
+
+func internalServerError(err error) (events.APIGatewayProxyResponse, error) {
+	fmt.Println("ERROR: ", err)
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusInternalServerError,
+	}, nil
 }
 
 func main() {
