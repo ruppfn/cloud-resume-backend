d01736a6d9350e4b06a992d3fd60dd75da685c19
Franco Nahuel Rupp <ruppfn@gmail.com>
2022-09-25 01:34:36 -0300

    attach required policies to the lambda role

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -3,7 +3,7 @@ data "aws_iam_policy_document" "lambda-logs" {
   version   = "2012-10-17"
   statement {
     effect  = "Allow"
-    actions = ["logs:CreateLogStream", "logs:PutLogEvents"]
+    actions = ["logs:CreateLogStream", "logs:PutLogEvents", "logs:CreateLogGroup"]
 
     resources = [
       "arn:aws:logs:${local.region}:${local.account_id}:log-group:/aws/lambda/${var.environment}*:*"
@@ -14,6 +14,33 @@ data "aws_iam_policy_document" "lambda-logs" {
 resource "aws_iam_policy" "logs" {
   name   = "${var.environment}-lambda-logs"
   policy = data.aws_iam_policy_document.lambda-logs.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_role_cloudwatch" {
+  policy_arn = aws_iam_policy.logs.arn
+  role       = aws_iam_role.lambda_role.name
+}
+
+data "aws_iam_policy_document" "lambda-dynamodb" {
+  policy_id = "${var.environment}-lambda-dynamodb"
+  version   = "2012-10-17"
+  statement {
+    effect    = "Allow"
+    actions   = ["dynamodb:GetItem"]
+    resources = [
+      "arn:aws:dynamodb:${local.region}:${local.account_id}:table/${aws_dynamodb_table.dynamo_table.name}"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "dynamodb" {
+  name   = "${var.environment}-lambda-dynamodb"
+  policy = data.aws_iam_policy_document.lambda-dynamodb.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_role_dynamo" {
+  policy_arn = aws_iam_policy.dynamodb.arn
+  role       = aws_iam_role.lambda_role.name
 }
 
 data "aws_iam_policy_document" "lambda_assume_role" {
