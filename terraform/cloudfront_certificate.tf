8bf07036450e6b271872a74a043098208cefb283
Franco Nahuel Rupp <ruppfn@gmail.com>
2022-09-25 20:34:44 -0300

    add us-east-1 acm certificate for cloudfront

---
+++ terraform/cloudfront_certificate.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+}
+
+resource "aws_acm_certificate" "cloudfront_cert" {
+  provider          = aws.us_east_1
+  domain_name       = local.web_domain_name
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_acm_certificate_validation" "cloudfront_validation" {
+  provider                = aws.us_east_1
+  certificate_arn         = aws_acm_certificate.cloudfront_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cloudfront_validation_record : record.fqdn]
+}
+
+resource "aws_route53_record" "cloudfront_validation_record" {
+  provider = aws.us_east_1
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = var.zone_id
+}
