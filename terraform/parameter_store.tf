10cd91a40792d777cc381b9348e61e37e8f94f0e
Franco Nahuel Rupp <ruppfn@gmail.com>
2023-01-05 19:35:40 -0300

    add parameter store to save github pat

---
+++ terraform/parameter_store.tf
@@ -0,0 +1,5 @@
+resource "aws_ssm_parameter" "github_pat" {
+  name  = "/${var.environment}/cloud-resume/github-pat"
+  type  = "SecureString"
+  value = "This is the real value... Trust me, im a dolphin"
+}
