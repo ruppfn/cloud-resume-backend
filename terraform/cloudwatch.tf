c1e4b546c75e74fc717c4794918d20968c95eb7e
Franco Nahuel Rupp <ruppfn@gmail.com>
2023-01-05 22:54:33 -0300

    add missing cloudwatch log groups

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -2,3 +2,13 @@ resource "aws_cloudwatch_log_group" "log" {
   name              = "/aws/${var.environment}"
   retention_in_days = 7
 }
+
+resource "aws_cloudwatch_log_group" "cv_lambda_group" {
+  name              = "/aws/lambda/${aws_lambda_function.cv_lambda.function_name}"
+  retention_in_days = 7
+}
+
+resource "aws_cloudwatch_log_group" "rebuild_lambda_group" {
+  name              = "/aws/lambda/${aws_lambda_function.rebuild_front_lambda.function_name}"
+  retention_in_days = 7
+}
