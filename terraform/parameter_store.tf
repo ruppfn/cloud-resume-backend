57a58c195fa8c6980d39a696d933bd1ae05587c4
Franco Nahuel Rupp <ruppfn@gmail.com>
2023-01-05 21:13:02 -0300

    add overwrite false to parameter store

--- terraform/parameter_store.tf
+++ terraform/parameter_store.tf
@@ -1,5 +1,6 @@
 resource "aws_ssm_parameter" "github_pat" {
-  name  = "/${var.environment}/cloud-resume/github-pat"
-  type  = "SecureString"
-  value = "This is the real value... Trust me, im a dolphin"
+  name      = "/${var.environment}/cloud-resume/github-pat"
+  type      = "SecureString"
+  value     = "This is the real value... Trust me, im a dolphin"
+  overwrite = false
 }
